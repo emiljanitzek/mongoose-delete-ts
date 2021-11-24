aac8337baff29e5ab647b3d76b5099346549a7a5
Emil Janitzek <emil@parakey.co>
2021-11-24 23:06:27 +0100

    refactor: create sperate file for DeletedQuery type and add to index

--- source/query.ts
+++ source/query.ts
@@ -1,11 +1,6 @@
-import { Query, Schema } from 'mongoose';
+import { Schema } from 'mongoose';
 import DeletedDocument from './DeletedDocument';
-
-export interface DeletedQuery<T extends DeletedDocument> {
-	withDeleted(): Query<T | null, T>
-	notDeleted(): Query<T | null, T>
-	onlyDeleted(): Query<T | null, T>
-}
+import DeletedQuery from "./DeletedQuery";
 
 export default function<T extends DeletedDocument>(schema: Schema): void {
 	schema.query.withDeleted = function(): DeletedQuery<T>['withDeleted'] {
