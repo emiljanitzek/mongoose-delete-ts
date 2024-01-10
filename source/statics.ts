77d130a11d7472ec00ca6b0f65c6fc3ae24b6d2c
stenneepro <stenneepro@gmail.com>
2024-01-10 06:43:29 -0500

    bump: Bump Mongoose to 8.0.4

--- source/statics.ts
+++ source/statics.ts
@@ -8,39 +8,117 @@ import {
 } from 'mongoose';
 
 import DeletedSchema from './types/DeletedSchema';
-import getOverloadedArguments from './utils/getOverloadedArguments';
+import getOverloadedArguments, { MongooseUpdateOptions } from './utils/getOverloadedArguments';
 import { staticDelete } from './utils/deleteDocument';
 import DeletedFieldOptions from './types/DeletedFieldOptions';
 import { staticRestore } from './utils/restoreDocument';
 
 export interface DeletedStaticMethods<T, TQueryHelpers={}> {
-	restoreOne(filter?: FilterQuery<T>, options?: QueryOptions | null): QueryWithHelpers<UpdateWriteOpResult, T, TQueryHelpers>;
-	restoreOne(filter?: FilterQuery<T>): QueryWithHelpers<UpdateWriteOpResult, T, TQueryHelpers>;
+	deleteMany(filter?: FilterQuery<T>, options?: MongooseUpdateOptions | null): QueryWithHelpers<
+		DeleteResult,
+		T,
+		TQueryHelpers,
+		T,
+		'deleteMany'
+	>;
+	deleteMany(filter: FilterQuery<T>): QueryWithHelpers<
+		DeleteResult,
+		T,
+		TQueryHelpers,
+		T,
+		'deleteMany'
+	>;
 
-	restoreMany(filter?: FilterQuery<T>, options?: QueryOptions | null): QueryWithHelpers<UpdateWriteOpResult, T, TQueryHelpers>;
-	restoreMany(filter?: FilterQuery<T>): QueryWithHelpers<UpdateWriteOpResult, T, TQueryHelpers>;
+	deleteOne(filter?: FilterQuery<T>, options?: MongooseUpdateOptions | null): QueryWithHelpers<
+		DeleteResult,
+		T,
+		TQueryHelpers,
+		T,
+		'deleteOne'
+	>;
+	deleteOne(filter?: FilterQuery<T>): QueryWithHelpers<
+		DeleteResult,
+		T,
+		TQueryHelpers,
+		T,
+		'deleteOne'
+	>;
+
+	restoreOne(filter?: FilterQuery<T>, options?: MongooseUpdateOptions | null): QueryWithHelpers<
+		UpdateWriteOpResult,
+		T,
+		TQueryHelpers,
+		T,
+		'updateOne'
+	>;
+	restoreOne(filter: FilterQuery<T>): QueryWithHelpers<
+		UpdateWriteOpResult,
+		T,
+		TQueryHelpers,
+		T,
+		'updateOne'
+	>;
+
+	restoreMany(filter?: FilterQuery<T>, options?: MongooseUpdateOptions | null): QueryWithHelpers<
+		UpdateWriteOpResult,
+		T,
+		TQueryHelpers,
+		T,
+		'updateMany'
+	>;
+	restoreMany(filter: FilterQuery<T>): QueryWithHelpers<
+		UpdateWriteOpResult,
+		T,
+		TQueryHelpers,
+		T,
+		'updateMany'
+	>;
 }
 
 export interface DeletedByStaticMethods<T, TUser = any, TQueryHelpers={}> {
-	deleteManyByUser(user: TUser, filter?: FilterQuery<T>, options?: QueryOptions): QueryWithHelpers<DeleteResult, T, TQueryHelpers>;
-	deleteManyByUser(user: TUser, filter: FilterQuery<T>): QueryWithHelpers<DeleteResult, T, TQueryHelpers>;
+	deleteManyByUser(user: TUser, filter?: FilterQuery<T>, options?: MongooseUpdateOptions | null): QueryWithHelpers<
+		DeleteResult,
+		T,
+		TQueryHelpers,
+		T,
+		'deleteMany'
+	>;
+	deleteManyByUser(user: TUser, filter: FilterQuery<T>): QueryWithHelpers<
+		DeleteResult,
+		T,
+		TQueryHelpers,
+		T,
+		'deleteMany'
+	>;
 
-	deleteOneByUser(user: TUser, filter?: FilterQuery<T>, options?: QueryOptions): QueryWithHelpers<DeleteResult, T, TQueryHelpers>;
-	deleteOneByUser(user: TUser, filter: FilterQuery<T>): QueryWithHelpers<DeleteResult, T, TQueryHelpers>;
+	deleteOneByUser(user: TUser, filter?: FilterQuery<T>, options?: MongooseUpdateOptions): QueryWithHelpers<
+		DeleteResult,
+		T,
+		TQueryHelpers,
+		T,
+		'deleteOne'
+	>;
+	deleteOneByUser(user: TUser, filter: FilterQuery<T>): QueryWithHelpers<
+		DeleteResult,
+		T,
+		TQueryHelpers,
+		T,
+		'deleteOne'
+	>;
 }
 
 export default function(
 	schema: DeletedSchema,
 	deletedFieldOptions: DeletedFieldOptions
 ): void {
-	schema.statics.deleteOne = async function<T>(filterArg?: FilterQuery<T>, optionsArg?: QueryOptions | null) {
+	schema.statics.deleteOne = async function<T>(filterArg?: FilterQuery<T>, optionsArg?: MongooseUpdateOptions | null) {
 		const [filter, options] = getOverloadedArguments(filterArg, optionsArg);
 
 		const update = staticDelete(deletedFieldOptions);
 		const result = await Model.updateOne.apply(this, [filter, update, options]);
 		return convertToDeleteResult(result);
 	};
-	schema.statics.deleteMany = async function<T>(filterArg?: FilterQuery<T>, optionsArg?: QueryOptions | null) {
+	schema.statics.deleteMany = async function<T>(filterArg?: FilterQuery<T>, optionsArg?: MongooseUpdateOptions | null) {
 		const [filter, options] = getOverloadedArguments(filterArg, optionsArg);
 
 		const update = staticDelete(deletedFieldOptions);
@@ -48,14 +126,14 @@ export default function(
 		return convertToDeleteResult(result);
 	};
 
-	schema.statics.deleteOneByUser = async function<TUser, T>(user: TUser, filterArg?: FilterQuery<T>, optionsArg?: QueryOptions | null) {
+	schema.statics.deleteOneByUser = async function<TUser, T>(user: TUser, filterArg?: FilterQuery<T>, optionsArg?: MongooseUpdateOptions | null) {
 		const [filter, options] = getOverloadedArguments(filterArg, optionsArg);
 
 		const update = staticDelete(deletedFieldOptions, user);
 		const result = await Model.updateOne.apply(this, [filter, update, options]);
 		return convertToDeleteResult(result);
 	};
-	schema.statics.deleteManyByUser = async function<TUser, T>(user: TUser, filterArg?: FilterQuery<T>, optionsArg?: QueryOptions | null) {
+	schema.statics.deleteManyByUser = async function<TUser, T>(user: TUser, filterArg?: FilterQuery<T>, optionsArg?: MongooseUpdateOptions | null) {
 		const [filter, options] = getOverloadedArguments(filterArg, optionsArg);
 
 		const update = staticDelete(deletedFieldOptions, user);
@@ -72,14 +150,14 @@ export default function(
 		return Model.findByIdAndUpdate.apply(this, [id, update, options] as any);
 	};
 
-	schema.statics.restoreOne = function<T>(filterArg?: FilterQuery<T>, optionsArg?: QueryOptions | null) {
+	schema.statics.restoreOne = function<T>(filterArg?: FilterQuery<T>, optionsArg?: MongooseUpdateOptions | null) {
 		const [filter, options] = getOverloadedArguments(filterArg, optionsArg);
 		Object.assign(options, { ignoreDeleted: true });
 
 		const update = staticRestore(deletedFieldOptions);
 		return Model.updateOne.apply(this, [filter, update, options]) as any;
 	};
-	schema.statics.restoreMany = function<T>(filterArg?: FilterQuery<T>, optionsArg?: QueryOptions | null) {
+	schema.statics.restoreMany = function<T>(filterArg?: FilterQuery<T>, optionsArg?: MongooseUpdateOptions | null) {
 		const [filter, options] = getOverloadedArguments(filterArg, optionsArg);
 		Object.assign(options, { ignoreDeleted: true });
 
@@ -91,6 +169,6 @@ export default function(
 function convertToDeleteResult(result: UpdateWriteOpResult): DeleteResult {
 	return {
 		acknowledged: result.acknowledged,
-		deletedCount: result.modifiedCount
+		deletedCount: result.modifiedCount,
 	};
 }
--- source/utils/getOverloadedArguments.ts
+++ source/utils/getOverloadedArguments.ts
@@ -1,9 +1,12 @@
-import { FilterQuery, QueryOptions } from 'mongoose';
+import { UpdateOptions } from 'mongodb';
+import { FilterQuery, MongooseSpecificQueryOptions } from 'mongoose';
+
+export interface MongooseUpdateOptions extends UpdateOptions, Omit<MongooseSpecificQueryOptions, 'lean'> {}
 
 export default function getOverloadedArguments<T>(
 	filter?: FilterQuery<T>,
-	options?: QueryOptions | null,
-): [FilterQuery<T>, QueryOptions] {
-	const queryOptions = Object.assign({}, options || {}, { timestamps: false });
-	return [filter || {}, queryOptions];
+	options?: MongooseUpdateOptions | null,
+): [FilterQuery<T>, MongooseUpdateOptions] {
+	const updateOptions = Object.assign({}, options || {}, { timestamps: false });
+	return [filter || {}, updateOptions];
 }
