a939e4b211ce2ebd517db2bcc14f1e786956218c
Emil Janitzek <emil@pixel2.se>
2025-01-08 09:45:23 +0100

    fix: update query helper to match any query operation

--- source/types/DeletedQuery.ts
+++ source/types/DeletedQuery.ts
@@ -1,7 +1,7 @@
 import { HydratedDocument, QueryWithHelpers } from 'mongoose';
 import Deleted from './Deleted';
 
-type DeletedQuery<T extends Deleted, TDoc = HydratedDocument<T, any, any>> = QueryWithHelpers<any, TDoc, DeletedQueryHelpers<T, TDoc>, any>;
+type DeletedQuery<T extends Deleted, TDoc = HydratedDocument<T, any, any>> = QueryWithHelpers<any, TDoc, DeletedQueryHelpers<T, TDoc>, any, any>;
 
 export interface DeletedQueryHelpers<T extends Deleted, TDoc = HydratedDocument<T, any, any>> {
 	allDocuments(this: DeletedQuery<T, TDoc>): this
